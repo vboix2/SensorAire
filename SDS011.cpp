24cbbe9ca4c2533d3fe3e676e8e4714786d19552
Víctor Boix <victorboix@protonmail.com>
2021-12-12 15:19:38 +0100

    wake up command

--- SDS011.cpp
+++ SDS011.cpp
@@ -32,6 +32,28 @@ static const byte SLEEPCMD[19] = {
 	0xAB	// tail
 };
 
+static const byte WAKECMD[19] = {
+	0xAA,	// head
+	0xB4,	// command id
+	0x06,	// data byte 1
+	0x01,	// data byte 2 (set mode)
+	0x01,	// data byte 3 (wake up)
+	0x00,	// data byte 4
+	0x00,	// data byte 5
+	0x00,	// data byte 6
+	0x00,	// data byte 7
+	0x00,	// data byte 8
+	0x00,	// data byte 9
+	0x00,	// data byte 10
+	0x00,	// data byte 11
+	0x00,	// data byte 12
+	0x00,	// data byte 13
+	0xFF,	// data byte 14 (device id byte 1)
+	0xFF,	// data byte 15 (device id byte 2)
+	0x06,	// checksum
+	0xAB	// tail
+};
+
 SDS011::SDS011(void) {
 
 }
@@ -92,9 +114,8 @@ void SDS011::sleep() {
 // SDS011:wakeup
 // --------------------------------------------------------
 void SDS011::wakeup() {
-	uint8_t wake_command[] = {0xAA, 0xB4, 0x06, 0x01, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0x06, 0xAB};
 	for (uint8_t i = 0; i < 19; i++) {
-		sds_data->write(wake_command[i]);
+		sds_data->write(WAKECMD[i]);
 	}
 	sds_data->flush();
 	while (sds_data->available() > 0) {
